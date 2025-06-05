bac702664217e07573ad65f673119337e916d235
lezwon <lezwon@users.noreply.github.com>
2025-06-05 08:13:44 +0000

    Deploying to gh-pages from @ lezwon/8085simulator@a3ac9d94d439de3ee0f9837a8ae31c71c401625e 🚀

--- utils/formatters.ts
+++
@@ -1,9 +0,0 @@
-
-export const toHexString = (value: number, length: number): string => {
-  return value.toString(16).toUpperCase().padStart(length, '0');
-};
-
-export const toBinaryString = (value: number, length: number): string => {
-  return value.toString(2).padStart(length, '0');
-};
-    
